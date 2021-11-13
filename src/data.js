6e7a065066f6b3167416ddba644c579f5c817b24
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-11-14 00:24:41 +0600

    useState practices

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const data = [
+  { id: 1, name: 'nadim' },
+  { id: 2, name: 'rakib' },
+  { id: 3, name: 'asif' },
+  { id: 4, name: 'sharif' },
+  { id: 5, name: 'akib' },
+  { id: 6, name: 'noman' },
+]
